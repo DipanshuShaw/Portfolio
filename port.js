5a8a111b7d22558648ac2f2227de2eeda40fc4d7
Dipanshu <dip100shaw@gmail.com>
2024-03-22 13:11:41 +0530

    Header completed photto not found

--- port.js
+++ port.js
@@ -1,1 +1,9 @@
 console.log("Hello World");
+
+// console.log(changingText.textContent);
+skillSet = ["Web Developer", "JAVA Programmer", "Python Programmer", "UI/UX Designer"]
+changingTextSpan = document.getElementById("header-skill-text");
+let idx = 0;
+setInterval(()=>{
+        changingTextSpan.textContent = skillSet[idx++ % skillSet.length];
+}, 2000)
